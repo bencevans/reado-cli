f9f926ab37635c8d39ac075c5552afae35aba882
Ben Evans <ben@bensbit.co.uk>
2016-07-01 17:46:32 +0100

    test

--- test.js
+++ test.js
@@ -12,11 +12,10 @@ test('runs with stdinput', (t) => {
 })
 
 test('--unfluff', (t) => {
-  t.plan(1)
+  t.plan(4)
   exec('cat test-html.html | ./bin.js', (err, stdout, stderr) => {
-    console.log(stdout)
     t.error(err)
-    t.assert(stdout.match(/score\: [0-9]+/), 'shows score')
+    t.assert(stdout.match(/score\: [0-9]+.+/), 'shows score')
     t.assert(stdout.match(/school level\: +/), 'shows school level')
     t.assert(stdout.match(/notes\: .+/), 'shows notes')
   })
